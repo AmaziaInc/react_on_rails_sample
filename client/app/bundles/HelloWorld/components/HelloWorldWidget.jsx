bba295f7d30b6fb81792a381036f6059f8e1b2b7
Eguchi Motoaki <eguchi@amazia.co.jp>
2016-09-14 10:23:39 +0900

    Reduxサンプル追加

--- client/app/bundles/HelloWorld/components/HelloWorldWidget.jsx
+++ client/app/bundles/HelloWorld/components/HelloWorldWidget.jsx
@@ -3,31 +3,8 @@
 
 import React, { PropTypes } from 'react';
 
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Paper from 'material-ui/Paper';
-import { FormsyCheckbox, FormsyDate, FormsyRadio, FormsyRadioGroup, 
-  FormsySelect, FormsyText, FormsyTime, FormsyToggle } from 'formsy-material-ui/lib';
-
 // Simple example of a React "dumb" component
 export default class HelloWorldWidget extends React.Component {
-  constructor(props) {
-    super(props)
-    this.styles = {
-      paperStyle: {
-        width: 300,
-        margin: 'auto',
-        padding: 20,
-      },
-      switchStyle: {
-        marginBottom: 16,
-      },
-      submitStyle: {
-        marginTop: 32,
-      },
-    };
-    this.handleChange = this.handleChange.bind(this)
-  }
   static propTypes = {
     // If you have lots of data or action properties, you should consider grouping them by
     // passing two properties: "data" and "actions".
@@ -43,27 +20,23 @@ export default class HelloWorldWidget extends React.Component {
 
   render() {
     const { name } = this.props;
-    let {paperStyle, switchStyle, submitStyle } = this.styles;
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme()}>
-        <Paper style={paperStyle}>
-          <div className="container">
-            <h3>
-              Hello, {name}!
-            </h3>
-            <hr />
-            <Formsy.Form>
-              <FormsyText
-                name="name"
-                type="text"
-                floatingLabelText="Name"
-                value={name}
-                onChange={this.handleChange}
-              />
-            </Formsy.Form>
-          </div>
-        </Paper>
-      </MuiThemeProvider>
+      <div className="container">
+        <h3>
+          Hello, {name}!
+        </h3>
+        <hr />
+        <form className="form-horizontal">
+          <label>
+            Say hello to:
+          </label>
+          <input
+            type="text"
+            value={name}
+            onChange={e => this.handleChange(e)}
+          />
+        </form>
+      </div>
     );
   }
 }
